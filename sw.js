23a5236aa68fc4910314d45bd16231a8b2b2c963
boody1000 <hassanboody906@gmail.com>
2026-09-02 22:34:08 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function(clientList) {
+            for (var i = 0; i < clientList.length; i++) {
+                var client = clientList[i];
+                if ('focus' in client) {
+                    return client.focus();
+                }
+            }
+            if (clients.openWindow) {
+                return clients.openWindow('./');
+            }
+        })
+    );
+});
